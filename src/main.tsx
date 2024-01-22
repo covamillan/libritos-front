1a95a35f4396b5f325ee4d4410fea4d10c057184
CovaM <cova.millan@gmail.com>
2024-01-22 13:33:35 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './reset.scss';
+import './global.scss';
+
+ReactDOM.createRoot(document.querySelector('.root')!).render(<App />);
